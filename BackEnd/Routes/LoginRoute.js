695eb13b4dfe59262a561dc5df3446984e3a5e79
ReemAtman <reematman15@gmail.com>
2023-08-29 18:38:33 +0300

    Editing Projects Files

--- BackEnd/Routes/LoginRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const jwt = require("jsonwebtoken")
-const Login = require("../Controllers/LoginController")
-let LoginRoute = express.Router()
-
-LoginRoute.route("/Login").post(Login)
-module.exports = LoginRoute
